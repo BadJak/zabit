27ff1a57386629b082c2d14ca4c4b8c5bd90c337
Basil JelalEldean <soli.geu@gmail.com>
2019-08-19 18:25:51 +0100

    frontend

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,10 @@
 import "bootstrap";
+import main from 'main'
+import mapCustom from 'map-custom'
+import slickCustom from 'slick-custom'
+
+
+
+main(jQuery);
+mapCustom(jQuery);
+slickCustom(jQuery);
